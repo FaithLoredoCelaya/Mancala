eb938b365dd5ca00781e4c53d088e5fc6b1f4bf1
Faith Loredo <Faithc.2555@gmail.com>
2024-07-10 21:24:01 -0400

    Managed to implement basic gamplay features. Just missing the steal mechanic from another player and as well the second player

--- Mancala/Classes/BoardSetUp.cs
+++ Mancala/Classes/BoardSetUp.cs
@@ -10,14 +10,50 @@ namespace Mancala.Classes
 {
     public class BoardSetUp
     {
-        int totalPeebles = 48;
+        public int totalPeebles = 48;
 
-        int peeblePerCup = 4;   
+        public int peeblePerCup = 4;
+        int playerTurn = 0;
+        int[] pocket = new int[12];
+        int[] scorePocket = new int[2];
+
+
+        public int[] getPocketScores()
+        {
+            return pocket;
+        }
+
+        public int[] getScorePockets()
+        {
+            return scorePocket;
+        }
+
+        public void switchPlayer()
+        {
+            if (playerTurn == 0)
+            {
+                playerTurn = 1;
+            }
+            else
+            {
+                playerTurn = 0;
+            }
+        }
 
         //Fills the cup with the peebles per cup as indicated by the limits avobe
-        public string fillCup()
+        public void  fillCup()
         {
-            return peeblePerCup.ToString();
+            for (int i=0;i<12;i++)
+            {
+                pocket[i] = peeblePerCup;
+            }
+        }
+
+        public void resetGame()
+        {
+            fillCup();
+            scorePocket[0] = 0;
+            scorePocket[1] = 0;
         }
 
         //Checks if the peebles are under the limit set up avobe
@@ -32,7 +68,43 @@ namespace Mancala.Classes
                 return false;
             }
         }
-        
+
+
+        //Updates the pockets. moving the pebbles one at at time to the next pocket just like in the real game.
+        public void  updateValues(int intial,int turn)
+        {
+            //set up the number of pebbles we need to move
+            int toMove = pocket[intial];
+            //See which pocket we are in
+            int counter = intial;
+            //empty the initial pocket
+            pocket[intial] = 0;
+            //move to the next pocket
+            counter++;
+            for (int i = 0; i <toMove; i++)
+            {
+                //Check if the pocket is a score pocket
+                if (counter == 6 || counter == 11)
+                {
+                    //Add score if it is
+                    scorePocket[turn]++;
+                }
+                else
+                {
+                    //Else just add one to the next pocket
+                    pocket[counter] = pocket[counter]+1;
+                }
+
+                counter++;
+                
+                //if the counter is more than the avialble pockets. reset to zero to loop around
+                if(counter==12)
+                {
+                    counter = 0;
+                }
+
+            }
+        }
     }
 
 }
--- Mancala/Form1.cs
+++ Mancala/Form1.cs
@@ -14,16 +14,19 @@ namespace Mancala
 {
     public partial class Form1 : Form
     {
+        //Create new game
         public BoardSetUp setBUp = new BoardSetUp();
-        public int spacesToMove = 0;
-        public int playerTurn = 1;
 
+        //Have local copy facilitate data transfer
+        int[] pockets = new int[12];
+        int[] scorePocket = new int[2];
 
         public Form1()
         {
             InitializeComponent();
-            ResetGame(setBUp);
-            Turn.Text = "Player Turn: " + playerTurn;
+            setBUp.fillCup();
+            updateDispaly();
+            Turn.Text = "Player Turn: 1";
         }
 
         private void label1_Click(object sender, EventArgs e)
@@ -38,55 +41,59 @@ namespace Mancala
 
         private void p1Pocket1_Click(object sender, EventArgs e)
         {
-            spacesToMove = Int32.Parse(p1Pocket1.Text);
-            playerTurn=2;
-            Turn.Text = "Player Turn: 2";
+            setBUp.updateValues(0,0);
+            pockets= setBUp.getPocketScores();
+            scorePocket = setBUp.getScorePockets();
+            updateDispaly();
+           // setBUp.switchPlayer();
+            //Turn.Text = "Player Turn: 2";
 
         }
 
         private void p1Pocket2_Click(object sender, EventArgs e)
         {
-
+            setBUp.updateValues(1, 0);
+            pockets = setBUp.getPocketScores();
+            scorePocket = setBUp.getScorePockets();
+            updateDispaly();
+         //   setBUp.switchPlayer();
+         //   Turn.Text = "Player Turn: 2";
         }
 
         private void StartNew_Click(object sender, EventArgs e)
         {
             ResetGame(setBUp);
+
         }
 
+        //changes text fields to show current values on the board
+        private void updateDispaly()
+        {
+            p1Pocket1.Text = pockets[0].ToString();
+            p1Pocket2.Text = pockets[1].ToString();
+            p1Pocket3.Text = pockets[2].ToString();
+            p1Pocket4.Text = pockets[3].ToString();
+            p1Pocket5.Text = pockets[4].ToString();
+            p1Pocket6.Text = pockets[5].ToString();
+            scoreP1.Text = scorePocket[0].ToString();
+
+            p2Pocket1.Text = pockets[6].ToString();
+            p2Pocket2.Text = pockets[7].ToString();
+            p2Pocket3.Text = pockets[8].ToString();
+            p2Pocket4.Text = pockets[9].ToString();
+            p2Pocket5.Text = pockets[10].ToString();
+            p2Pocket6.Text = pockets[11].ToString();
+            scoreP2.Text = scorePocket[1].ToString();
+        }
         private void ResetGame(BoardSetUp setUp)
         {
             //Resets board and fills pockets with peebles
-            p1Pocket1.Text = setBUp.fillCup();
-            p1Pocket2.Text = setBUp.fillCup();
-            p1Pocket3.Text = setBUp.fillCup();
-            p1Pocket4.Text = setBUp.fillCup();
-            p1Pocket5.Text = setBUp.fillCup();
-            p1Pocket6.Text = setBUp.fillCup();
-
-            p2Pocket1.Text = setBUp.fillCup();
-            p2Pocket2.Text = setBUp.fillCup();
-            p2Pocket3.Text = setBUp.fillCup();
-            p2Pocket4.Text = setBUp.fillCup();
-            p2Pocket5.Text = setBUp.fillCup();
-            p2Pocket6.Text = setBUp.fillCup();
+            setBUp.resetGame();
+            pockets= setBUp.getPocketScores();
+            scorePocket = setBUp.getScorePockets();
+            updateDispaly();
         }
 
-        private void UpdatePeebles(int toMove, string p, int pocket)
-        {
-            if (p == "p1")
-            {
-/*                switch (pocket)
-                {
-              case 1:
-                        int result = Int32.Parse(p1Pocket1.Text) - 1;
-                        p1Pocket1.Text = result;
-                    default:      
-                    
-                }*/
-            }
-            
-        }
 
     }
 }
